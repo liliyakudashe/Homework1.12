731e6012312bf40e067fcf0228e92c2c4b82c370
Acer <oduvan4ik_888@mail.ru>
2022-09-30 01:06:51 +0300

    на проверку

---
+++ src/Author.java
@@ -0,0 +1,15 @@
+public class Author {
+    private String authorBook;
+
+    public Author(String authorOfTheBook){
+       this.authorBook = authorOfTheBook;
+    }
+
+    public String getAuthorBook() {
+        return authorBook;
+    }
+
+    public void setAuthorBook(String authorBook) {
+        this.authorBook = authorBook;
+    }
+}
---
+++ src/Book.java
@@ -0,0 +1,26 @@
+public class Book {
+    private String titleOfTheBook;
+    private String yearOfPublicationOfTheBook;
+    private Author authorBook;
+
+    public Book(String titleOfTheBook, String yearOfPublicationOfTheBook, String authorOfTheBook){
+        this.titleOfTheBook = titleOfTheBook;
+        this.yearOfPublicationOfTheBook = yearOfPublicationOfTheBook;
+        this.authorBook = authorBook;
+    }
+    public String getTitleOfTheBook(){
+        return this.titleOfTheBook;
+    }
+    public String getYearOfPublicationOfTheBook(){
+        return this.yearOfPublicationOfTheBook;
+    }
+
+    public void setYearOfPublicationOfTheBook(String yearOfPublicationOfTheBook){
+        this.yearOfPublicationOfTheBook = yearOfPublicationOfTheBook;
+    }
+
+    public String getAuthorBook(){
+        return String.valueOf(this.authorBook);
+    }
+
+}
